80ddad9a6f7196c610339aa4936cd9195e396751
Tarynleewalbrugh25 <walbrught@gmail.com>
2023-12-20 14:25:10 +0200

    Updates

---
+++ src/components/Footer.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="container">
+        <div class="row">
+            <p class="lead">
+                Taryn-lee's Portfolio</p>
+            <span>{{ currYear }}</span>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'FooterComp',
+        data() {
+            return {
+                currYear: new Date().getFullYear()
+            } 
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
